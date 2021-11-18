2c2c32d6d964190bef9de0a36d66ba69825d37cf
Kristine Laing <kristal.tasha@gmail.com>
2021-11-18 14:58:19 +0000

    navbar, humburger menu, firstscreen slogan done

---
+++ index.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("ul");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".navlink");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+// $(window).scroll(function(){
+//     if($(window).scrollTop()){
+//         $("header")
+//     }
+// }
+//     )
